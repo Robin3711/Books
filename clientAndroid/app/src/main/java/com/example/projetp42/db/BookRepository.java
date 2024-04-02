ed0934fa9bbde762b8ddb492bd3cc2d49743fc53
miniotti <robin.miniotti@etu.unistra.fr>
2024-04-02 11:11:47 +0200

    added the xml for the filterings options

--- clientAndroid/app/src/main/java/com/example/projetp42/db/BookRepository.java
+++ clientAndroid/app/src/main/java/com/example/projetp42/db/BookRepository.java
@@ -21,8 +21,8 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class BookRepository {
 
-    //private static final String BASE_URL = "http://192.168.122.1:3000/";
-    private static final String BASE_URL = "http://192.168.1.125:3000/";// URL Arnaud
+    private static final String BASE_URL = "http://10.0.2.2:3000/";
+    //private static final String BASE_URL = "http://192.168.1.125:3000/";// URL Arnaud
 
 
     public BookRepository() {
