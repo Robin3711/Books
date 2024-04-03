6a6f83cd4f1befc712d737bd185fc6c22427e4a0
miniotti <robin.miniotti@etu.unistra.fr>
2024-04-03 20:01:34 +0200

    add author now working

--- clientAndroid/app/src/main/java/com/example/projetp42/db/AuthorRepository.java
+++ clientAndroid/app/src/main/java/com/example/projetp42/db/AuthorRepository.java
@@ -1,13 +1,17 @@
 package com.example.projetp42.db;
 
 import android.content.Context;
+import android.util.Log;
 
+import com.android.volley.Request;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.projetp42.model.Author;
 import com.example.projetp42.viewmodel.author.AuthorsViewModel;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -33,7 +37,7 @@ public class AuthorRepository {
         VolleyRequestQueue.getInstance(context).add(request);
     }
 
-    public ArrayList<Author> jsonToAuthors(JSONArray json) {
+    public ArrayList<Author> jsonToAuthors( JSONArray json) {
         ArrayList<Author> authors = new ArrayList<>();
         try {
             for (int i = 0; i < json.length(); i++) {
@@ -48,4 +52,48 @@ public class AuthorRepository {
         }
         return authors;
     }
+
+    public void addAuthor(Context context, Author author, AddAuthorCallback callback) {
+        String url = BASE_URL + "authors";
+
+        JSONObject json = new JSONObject();
+        try {
+            json.put("firstname", author.firstname);
+            json.put("lastname", author.lastname);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            if (callback != null) {
+                callback.onFailure("Error creating JSON object");
+            }
+            return;
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, json,
+                response -> {
+                    try {
+                        int authorId = response.getInt("id");
+                        if (callback != null) {
+                            callback.onSuccess(authorId);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        if (callback != null) {
+                            callback.onFailure("Error parsing server response");
+                        }
+                    }
+                },
+                error -> {
+                    if (callback != null) {
+                        callback.onFailure("Error adding author: " + error.getMessage());
+                    }
+                });
+
+        VolleyRequestQueue.getInstance(context).add(jsonObjectRequest);
+    }
+
+    public interface AddAuthorCallback {
+        void onSuccess(int authorId);
+        void onFailure(String errorMessage);
+    }
+
 }
